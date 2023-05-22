a596a67df81b3810304d28c67dae47c0b74887e6
Go6u <51147693+Go6u@users.noreply.github.com>
2023-05-22 18:28:43 +0300

    Add files via upload

---
+++ Football/Football/Program.cs
@@ -0,0 +1,83 @@
+﻿public abstract class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public abstract class FootballPlayer
+{
+    public string Name { get; set; }
+    public int Number { get; set; }
+    public int Age { get; set; }
+    public double Height { get; set; }
+
+}
+
+public class Goalkeeper : FootballPlayer
+{
+    public Goalkeeper(string name, int number, int age, double height)
+    {
+        this.Name = name;
+        this.Number = number;
+        this.Age = age;
+        this.Height = height;
+    }
+}
+
+public class Defender : FootballPlayer
+{
+    public Defender(string name, int number, int age, double height)
+    {
+        this.Name = name;
+        this.Number = number;
+        this.Age = age;
+        this.Height = height;
+    }
+}
+
+public class Midfield : FootballPlayer
+{
+    public Midfield(string name, int number, int age, double height)
+    {
+        this.Name = name;
+        this.Number = number;
+        this.Age = age;
+        this.Height = height;
+    }
+}
+
+public class Atacker : FootballPlayer
+{
+    public Atacker(string name, int number, int age, double height)
+    {
+        this.Name = name;
+        this.Number = number;
+        this.Age = age;
+        this.Height = height;
+    }
+}
+
+public class Coach : Person
+{
+    public Coach(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
+
+public class Referee : Person
+{
+    public Referee(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
